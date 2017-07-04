1502c8c5f397bfd18e9c5d91a85e3411b99d1b9d
kunisada <miriev56@gmail.com>
2017-07-04 12:06:45 +0900

    scss file and gulp task runner added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('default', function() {
-  gulp.src('_layouts/css/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('_layouts/css'));
+
+gulp.task('default', function(){
+	gulp.src('assets/scss/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('assets/css'));
 });
