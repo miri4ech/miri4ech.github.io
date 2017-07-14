48104d4c774c8b27d878d8d6b40369897d77c7da
Miri Kunisada <miriev56@gmail.com>
2017-07-14 12:08:03 +0900

    modified css files

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-	gulp.src('assets/_sass/main.scss')
+	gulp.src('assets/_sass/custom.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('assets/_sass/main.scss', ['sass']);
+  gulp.watch('assets/_sass/custom.scss', ['sass']);
 });
